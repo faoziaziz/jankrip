5fddab52a178730a29c6a08e41a835794e7632e8
Aziz Amerul Faozi <cahalapisan@gmail.com>
2018-06-01 00:09:23 +0700

    jangkrip

--- src/main/java/com/labseni/jangkrik/JangkrikApplication.java
+++ src/main/java/com/labseni/jangkrik/JangkrikApplication.java
@@ -1,21 +1,36 @@
 package com.labseni.jangkrik;
-
+/*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
+*/
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.*;
+
+import java.sql.*;
+import java.util.*;
+import java.net.URISyntaxException;
+import java.net.URI;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication
 public class JangkrikApplication {
 
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
-      return "Krik Krik Jangkrik";
+  //  @ResponseBody
+    String home(Model model) {
+      return "home";
     }
 
     public static void main(String[] args) {
         SpringApplication.run(JangkrikApplication.class, args);
     }
 }
+
+
